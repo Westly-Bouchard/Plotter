324b93cbf694b3c8a02981e9a7351259c54df6ee
Westly Bouchard <westly.bouchard@gmail.com>
2024-10-07 12:28:33 -0600

    Testing code for steppers

---
+++ hardware/include/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+/*** MOTOR CONTROL PINS ***/
+constexpr unsigned int X_AXIS_STEP = 2;
+constexpr unsigned int X_AXIS_DIR = 5;
+
+constexpr unsigned int Y_AXIS_STEP = 3;
+constexpr unsigned int Y_AXIS_DIR = 6;
+
+/*** MOTOR PARAMETERS ***/
+constexpr unsigned int X_AXIS_MAX_SPEED = 1000;
+constexpr unsigned int Y_AXIS_MAX_SPEED = 1000;
+
+#endif
--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
+#include "constants.h"
+
+AccelStepper xAxis(
+  1,
+  X_AXIS_STEP,
+  X_AXIS_DIR
+);
+
+AccelStepper yAxis(
+  1,
+  Y_AXIS_STEP,
+  Y_AXIS_DIR
+);
+
+MultiStepper gantry;
+
+long positions1[2] = {200, 100};
+long positions2[2] = {0, 0};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  xAxis.setMaxSpeed(X_AXIS_MAX_SPEED);
+
+  yAxis.setMaxSpeed(Y_AXIS_MAX_SPEED);
+
+  gantry.addStepper(xAxis);
+  gantry.addStepper(yAxis);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  gantry.moveTo(positions1);
+  gantry.runSpeedToPosition();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+
+  gantry.moveTo(positions2);
+  gantry.runSpeedToPosition();
+
+  delay(1000);
 }
