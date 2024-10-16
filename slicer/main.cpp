b610f877503be35f24397bb0c712942cfd017a4c
Westly Bouchard <westly.bouchard@gmail.com>
2024-10-16 09:03:10 -0600

    More work on the gui

--- slicer/main.cpp
+++ slicer/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * Entry point for slicer program
+ *
+ * Created by Westly Bouchard
+ */
+
+/**
+ * Includes for GUI Libraries
+ *
+ * Using raylib and raygui
+ */
 #include "raylib.h"
 
 #define RAYGUI_IMPLEMENTATION
@@ -5,33 +16,106 @@
 #include "raygui/src/raygui.h"
 #include "raygui/styles/bluish/style_bluish.h"
 
-int main() {
-    InitWindow(1200, 800, "testing");
+/**
+ * Includes for SVG parsing library
+ *
+ * Using: nanosvg and nanosvgrast
+ */
+#include "nanosvg/src/nanosvg.h"
+#include "nanosvg/src/nanosvgrast.h"
 
+/**
+ * Include for serial communication with Arduino
+ *
+ * I wrote this with the help of a GeeksForGeeks article about serial communication
+ */
+#include "serialUtils.hpp"
+
+using namespace std;
+
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 800;
+
+int main() {
+
+//    // For now this will just be hardcoded
+//    const char* portName = "/dev/cu.usbmodem1301";
+//
+//    // Open connection to Arduino and check for failure
+//    int fd = openSerialPort(portName);
+//
+//    if (fd < 0) {
+//        return 1;
+//    }
+//
+//    // Configure for correct communication protocol
+//    if (!configureSerialPort(fd, B9600)) {
+//        closeSerialPort(fd);
+//        return 1;
+//    }
+//
+//
+    // GUI Setup
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "It's Ambitious");
+
+    // Initial window position
+    const Vector2 windowPosition = { SCREEN_WIDTH / 2 - 200 / 2, SCREEN_HEIGHT / 2 - 465 / 2 };
+
+    // Load built in blue color scheme
     GuiLoadStyleBluish();
 
     SetTargetFPS(60);
 
-    bool showMessageBox = false;
+    bool btnLoadPressed = false;
 
+    bool imageLoaded = false;
+
+    // Container for image texture data
+    Texture2D texture = { 0 };
+
+    // Container for rendered image data
+    void* img;
+
+    // GUI Mainloop
     while (!WindowShouldClose()) {
-        BeginDrawing();
 
-        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+        /*
+         * Usage here is to just drag and drop a file onto the window because I don't want to deal with asking for
+         * a file picker modal from the operating system.
+         */
+        if (IsFileDropped()) {
+            FilePathList droppedFiles = LoadDroppedFiles();
 
-        if (GuiButton((Rectangle){24, 24, 120, 30}, "#191#Show Message")) showMessageBox = true;
+            // If the user correctly dropped ONE .svg file onto the window, we can proceed
+            if ((droppedFiles.count == 1) && IsFileExtension(droppedFiles.paths[0], ".svg")) {
+                // Load the vector data
+                NSVGimage * image;
 
-        if (showMessageBox) {
-            int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                                       "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+                image = nsvgParseFromFile(droppedFiles.paths[0], "px", 96);
 
-            if (result >= 0) showMessageBox = false;
+                // Have to actually render the image to show a preview in the window
+                struct NSVGrasterizer* rast = nsvgCreateRasterizer();
+
+                // Allocate memory for the image itself
+                img = malloc(100 * 100 * 4);
+
+                // Rasterize the image
+                nsvgRasterize(rast, image, 0, 0, 1, static_cast<unsigned char*>(img), 100, 100, 400);
+
+                Image raylibImage = LoadImageFromMemory()
+            }
+
+            UnloadDroppedFiles(droppedFiles);
         }
+
+        BeginDrawing();
 
         EndDrawing();
     }
 
     CloseWindow();
+
+//    closeSerialPort(fd);
 
     return 0;
 
---
+++ slicer/serialUtils.hpp
@@ -0,0 +1,77 @@
+//
+// Created by Westly Bouchard on 10/15/24.
+//
+
+#ifndef SLICER_SERIALUTILS_H
+#define SLICER_SERIALUTILS_H
+
+#include <cstring>
+#include <fcntl.h>
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+int openSerialPort(const char* portName) {
+    int fd = open(portName, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 1) {
+        std::cerr << "Error opening serial connection: " << portName << std::endl;
+        return -1;
+    }
+
+    return fd;
+}
+
+bool configureSerialPort(int fd, int speed) {
+    struct termios tty;
+    if (tcgetattr(fd, &tty) != 0) {
+        std::cerr << "Error from tcgetattr" << std::endl;
+        return false;
+    }
+
+    // Set baud rate
+    cfsetospeed(&tty, speed);
+    cfsetispeed(&tty, speed);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // Use 8-bit characters
+    tty.c_iflag &= ~IGNBRK; // Disable break processing
+    tty.c_lflag = 0; // No signaling chars, no echo, no canonical processing
+    tty.c_oflag = 0; // No remapping, no delays
+    tty.c_cc[VMIN] = 0; // Read doesn't block
+    tty.c_cc[VTIME] = 5; // 0.5 seconds for read timeout
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Shut off xon/xoff control
+
+    tty.c_cflag |= (CLOCAL | CREAD); // Ignore modem controls, enable reading
+
+    tty.c_cflag &= ~(PARENB | PARODD); // Shut off parity
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        std::cerr << "Error from tcsetattr" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+int readFromSerialPort(int fd, char *buffer, size_t size) {
+    return read(fd, buffer, size);
+}
+
+int writeToSerialPort(int fd, const char *buffer, size_t size) {
+    return write(fd, buffer, size);
+}
+
+void closeSerialPort(int fd) { close(fd); }
+
+// Example reference for how to read from the serial port
+//char buffer[100];
+//    int n = readFromSerialPort(fd, buffer, sizeof(buffer));
+//    if (n < 0) {
+//        cerr << "Error reading from serial port" << endl;
+//    } else {
+//        cout << "Read form serial port: " << string(buffer, n) << endl;
+//    }
+
+#endif //SLICER_SERIALUTILS_H
